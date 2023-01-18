9c4b66c6264961a277f0bb0a0fea9bcfdfcc03e7
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-18 15:10:28 +1300

    Added the printResults function
    Finished console version of this game by calling the printResults
    function and printing who won the round, and what the current score
    is.

--- script.js
+++ script.js
@@ -15,27 +15,27 @@ getComputerChoice = () => {
 playRockPaperScissors = (playerSelection, computerSelection) => {
     if (playerSelection.toLowerCase() == "rock") {
         if (computerSelection == "scissors") {
-            return `You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`
+            return "player"
         } else if (computerSelection == "paper") {
-            return `You Lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`
+            return "computer"
         } else {
-            return `It's a tie. Both chose ${computerSelection}`
+            return "draw"
         }
     } else if (playerSelection.toLowerCase() == "paper") {
         if (computerSelection == "rock") {
-            return `You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`
+            return "player"
         } else if (computerSelection == "scissors") {
-            return `You Lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`
+            return "computer"
         } else {
-            return `It's a tie. Both chose ${computerSelection}`
+            return "draw"
         }
     } else if (playerSelection.toLowerCase() == "scissors") {
         if (computerSelection == "paper") {
-            return `You win! ${playerSelection.toLowerCase()} beats ${computerSelection}`
+            return "player"
         } else if (computerSelection == "rock") {
-            return `You Lose! ${computerSelection} beats ${playerSelection.toLowerCase()}`
+            return "computer"
         } else {
-            return `It's a tie. Both chose ${computerSelection}`
+            return "draw"
         }
     }
 }
@@ -51,14 +51,25 @@ updateScore = (result) => {
     } 
 }
 
+/* printResults is a function that prints the winner of the round (or states if it were a tie) and 
+    lets the user know the current score.  */ 
 
+printResults = (result, playerScore, computerScore) => {
+    if (result == "player") {
+        console.log(`You have won this round! Player score: ${playerScore} Computer score: ${computerScore}`);
+    } else if (result == "computer") {
+        console.log(`The computer won this round! Player score: ${playerScore} Computer score: ${computerScore}`);
+    } else if (result == "draw") {
+        console.log(`It was a tie. Player score: ${playerScore} Computer score: ${computerScore}`);
+    }
+}
 game = () => {
     for (let i = 0; i < 5; i++) {
         let playerSelection = prompt("Choose rock, paper, or scissors: ");
         let computerSelection = getComputerChoice();
         let result = playRockPaperScissors(playerSelection, computerSelection);
         updateScore(result);
-
+        printResults(result, playerScore, computerScore);
     }
 }
 
